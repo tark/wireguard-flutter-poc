524e680fae65222d9c34c9030b99ec8d3a80287a
Airon <airon@tark.pro>
2021-04-03 13:36:54 +0700

    MainActivity.kt: fixes bugs, implement stats callback

--- android/app/src/main/kotlin/com/example/wireguard_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wireguard_flutter/MainActivity.kt
@@ -5,27 +5,21 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import com.beust.klaxon.Klaxon
 import com.wireguard.android.backend.*
 import com.wireguard.android.util.ModuleLoader
 import com.wireguard.android.util.RootShell
 import com.wireguard.android.util.ToolsInstaller
 import com.wireguard.config.Config
-import com.wireguard.config.InetNetwork
 import com.wireguard.config.Interface
 import com.wireguard.config.Peer
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.*
-import java.lang.ref.WeakReference
-import java.net.InetAddress
 import java.util.*
 
 class MainActivity : FlutterActivity() {
-
-  /*private val permissionActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-    toggleTunnelWithPermissionsResult()
-  }*/
 
   private val permissionRequestCode = 10014
   private val channel = "tark.pro/wireguard-flutter"
@@ -36,153 +30,11 @@ class MainActivity : FlutterActivity() {
   private lateinit var rootShell: RootShell
   private lateinit var toolsInstaller: ToolsInstaller
   private var havePermission = false
+  private var methodChannel: MethodChannel? = null
 
-  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-    super.configureFlutterEngine(flutterEngine)
-    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler { call, result ->
-      scope.launch {
-
-        if (!havePermission) {
-          result.error("Have no permission to configure VPN", null, null)
-          return@launch
-        }
-
-        when (call.method) {
-          "getTunnelNames" -> {
-            try {
-              val names = futureBackend.await().runningTunnelNames
-              Log.i(TAG, "Success $names")
-              result.success(names.toString())
-            } catch (e: Throwable) {
-              Log.e(TAG, "Can't get tunnel names: " + e.message + " " + e.stackTrace)
-              result.error("Can't get tunnel names", null, null)
-            }
-          }
-          "setState" -> {
-            try {
-
-              val config = Config.Builder()
-                .setInterface(
-                  Interface.Builder()
-                    .parseAddresses("10.200.200.185")
-                    .parseListenPort("51820")
-                    .parseDnsServers("116.203.231.122")
-                    .parsePrivateKey("mIWKevXKBlBxXEAtzJJtLOU0TjSduvvm9rUQpvdPBkM=")
-                    .build(),
-                )
-                .addPeer(
-                  Peer.Builder()
-                    .parseAllowedIPs("0.0.0.0/0")
-                    .parsePublicKey("9Xhc/RmDmmyy54+F/mhSh1KEV0/bjD6ruAp934pmlDk=")
-                    .parseEndpoint("wghongkong01.spidervpnservers.com:443")
-                    .build()
-                )
-                .build()
-
-              Log.i(TAG, "onOptionsItemSelected interface - ${config.`interface`.addresses}")
-              Log.i(TAG, "onOptionsItemSelected interface - ${config.`interface`.dnsServers}")
-              Log.i(TAG, "onOptionsItemSelected interface - ${config.`interface`.keyPair.privateKey.toBase64()}")
-              Log.i(TAG, "onOptionsItemSelected interface - ${config.`interface`.keyPair.publicKey.toBase64()}")
-              Log.i(TAG, "onOptionsItemSelected interface - ${config.`interface`.listenPort.get()}")
-              Log.i(TAG, "onOptionsItemSelected -------")
-              Log.i(TAG, "onOptionsItemSelected peer - ${config.peers[0].allowedIps}")
-              Log.i(TAG, "onOptionsItemSelected peer - ${config.peers[0].endpoint.get()}")
-              Log.i(TAG, "onOptionsItemSelected peer - ${config.peers[0].publicKey.toBase64()}")
-              //scope.launch {
-                futureBackend.await().setState(MyTunnel("example-tunnel"), Tunnel.State.TOGGLE, config)
-                Log.i(TAG, "setState - success!")
-                result.success("setState - success!")
-              //}
-
-            } catch (e: BackendException) {
-              Log.e(TAG, "setState - BackendException - ERROR - ${e.reason}")
-              result.error("setState - BackendException - ERROR - ${e.reason}", null, null)
-            } catch (e: Throwable) {
-              Log.e(TAG, "Can't set tunnel state: $e")
-              result.error("setState - ERROR - ${e.message}", null, null)
-            }
-          }
-          else -> {
-            result.notImplemented()
-          }
-        }
-      }
-    }
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    Log.i(TAG, "onActivityResult - $requestCode - $resultCode")
-    super.onActivityResult(requestCode, resultCode, data)
-    havePermission = (requestCode == permissionRequestCode) && (resultCode == Activity.RESULT_OK)
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Log.i(TAG, "onCreate")
-    rootShell = RootShell(applicationContext)
-    toolsInstaller = ToolsInstaller(applicationContext, rootShell)
-    moduleLoader = ModuleLoader(applicationContext, rootShell, USER_AGENT)
-
-    scope.launch(Dispatchers.IO) {
-      try {
-        backend = determineBackend()
-        futureBackend.complete(backend!!)
-        checkPermission()
-      } catch (e: Throwable) {
-        Log.e(TAG, Log.getStackTraceString(e))
-      }
-    }
-  }
-
-  private fun determineBackend(): Backend {
-    Log.i(TAG, "determineBackend")
-    var backend: Backend? = null
-    var didStartRootShell = false
-    if (!ModuleLoader.isModuleLoaded() && moduleLoader.moduleMightExist()) {
-      try {
-        rootShell.start()
-        didStartRootShell = true
-        moduleLoader.loadModule()
-      } catch (ignored: Exception) {
-        Log.e(TAG, Log.getStackTraceString(ignored))
-      }
-    }
-    if (/*!UserKnobs.disableKernelModule.first() &&*/ ModuleLoader.isModuleLoaded()) {
-      try {
-        if (!didStartRootShell) {
-          rootShell.start()
-        }
-        val wgQuickBackend = WgQuickBackend(applicationContext, rootShell, toolsInstaller)
-        //wgQuickBackend.setMultipleTunnels(UserKnobs.multipleTunnels.first())
-        backend = wgQuickBackend
-        Log.i(TAG, "--> WG QUICK BACKEND <--")
-
-        // what is that? I totally did not understand
-        /*UserKnobs.multipleTunnels.onEach {
-          wgQuickBackend.setMultipleTunnels(it)
-        }.launchIn(coroutineScope)*/
-      } catch (ignored: Exception) {
-        Log.e(TAG, Log.getStackTraceString(ignored))
-      }
-    }
-    if (backend == null) {
-      Log.i(TAG, "--> GO BACKEND <--")
-      backend = GoBackend(applicationContext)
-      // the blow line is very important, it returns the callback from the GoBackend
-      //GoBackend.setAlwaysOnCallback { get().applicationScope.launch { get().tunnelManager.restoreState(true) } }
-    }
-    return backend
-  }
-
-  private fun checkPermission() {
-    val intent = GoBackend.VpnService.prepare(this)
-    if (intent != null) {
-      havePermission = false
-      startActivityForResult(intent, permissionRequestCode)
-    } else {
-      havePermission = true
-    }
-  }
+  // Have to keep tunnels, because WireGuard requires to use the _same_
+  // instance of a tunnel every time you change the state.
+  private var tunnels = HashMap<String, Tunnel>()
 
   companion object {
     const val TAG = "MainActivity"
@@ -197,45 +49,253 @@ class MainActivity : FlutterActivity() {
       Build.MODEL,
       Build.FINGERPRINT
     )
+  }
 
-    private lateinit var weakSelf: WeakReference<MainActivity>
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel)
+    methodChannel?.setMethodCallHandler { call, result ->
 
-    @JvmStatic
-    fun get(): MainActivity {
-      return weakSelf.get()!!
+      if (!havePermission) {
+        flutterError(result, "Have no permission to configure VPN")
+        return@setMethodCallHandler
+      }
+
+      when (call.method) {
+        "getTunnelNames" -> handleGetNames(result)
+        "setState" -> handleSetState(call.arguments, result)
+        "getStats" -> handleGetStats(call.arguments, result)
+        else -> flutterNotImplemented(result)
+      }
+
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    havePermission = (requestCode == permissionRequestCode) && (resultCode == Activity.RESULT_OK)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    rootShell = RootShell(applicationContext)
+    toolsInstaller = ToolsInstaller(applicationContext, rootShell)
+    moduleLoader = ModuleLoader(applicationContext, rootShell, USER_AGENT)
+
+    scope.launch(Dispatchers.IO) {
+      try {
+        backend = createBackend()
+        futureBackend.complete(backend!!)
+        checkPermission()
+      } catch (e: Throwable) {
+        Log.e(TAG, Log.getStackTraceString(e))
+      }
+    }
+  }
+
+  //
+  private fun createBackend(): Backend {
+    var backend: Backend? = null
+    var didStartRootShell = false
+    if (!ModuleLoader.isModuleLoaded() && moduleLoader.moduleMightExist()) {
+      try {
+        rootShell.start()
+        didStartRootShell = true
+        moduleLoader.loadModule()
+      } catch (ignored: Exception) {
+        Log.e(TAG, Log.getStackTraceString(ignored))
+      }
+    }
+    if (ModuleLoader.isModuleLoaded()) {
+      try {
+        if (!didStartRootShell) {
+          rootShell.start()
+        }
+        val wgQuickBackend = WgQuickBackend(applicationContext, rootShell, toolsInstaller)
+        //wgQuickBackend.setMultipleTunnels(UserKnobs.multipleTunnels.first())
+        backend = wgQuickBackend
+        // what is that? I totally did not understand
+        /*UserKnobs.multipleTunnels.onEach {
+          wgQuickBackend.setMultipleTunnels(it)
+        }.launchIn(coroutineScope)*/
+      } catch (ignored: Exception) {
+        Log.e(TAG, Log.getStackTraceString(ignored))
+      }
+    }
+    if (backend == null) {
+      backend = GoBackend(applicationContext)
+    }
+    return backend
+  }
+
+  private fun checkPermission() {
+    val intent = GoBackend.VpnService.prepare(this)
+    if (intent != null) {
+      havePermission = false
+      startActivityForResult(intent, permissionRequestCode)
+    } else {
+      havePermission = true
+    }
+  }
+
+  private fun flutterSuccess(result: MethodChannel.Result, o: Any) {
+    scope.launch(Dispatchers.Main) {
+      result.success(o)
+    }
+  }
+
+  private fun flutterError(result: MethodChannel.Result, error: String) {
+    scope.launch(Dispatchers.Main) {
+      result.error(error, null, null)
+    }
+  }
+
+  private fun flutterNotImplemented(result: MethodChannel.Result) {
+    scope.launch(Dispatchers.Main) {
+      result.notImplemented()
+    }
+  }
+
+  private fun handleSetState(arguments: Any, result: MethodChannel.Result) {
+
+    scope.launch(Dispatchers.IO) {
+      try {
+
+        val params = Klaxon().parse<SetStateParams>(arguments.toString())
+
+        if (params == null) {
+          flutterError(result, "Set state params is missing")
+          return@launch
+        }
+
+        val config = Config.Builder()
+          .setInterface(
+            Interface.Builder()
+              .parseAddresses(params.tunnel.address)
+              .parseListenPort(params.tunnel.listenPort)
+              .parseDnsServers(params.tunnel.dnsServer)
+              .parsePrivateKey(params.tunnel.privateKey)
+              .build(),
+          )
+          .addPeer(
+            Peer.Builder()
+              .parseAllowedIPs(params.tunnel.peerAllowedIp)
+              .parsePublicKey(params.tunnel.peerPublicKey)
+              .parseEndpoint(params.tunnel.peerEndpoint)
+              .build()
+          )
+          .build()
+        //futureBackend.await().setState(MyTunnel(params.tunnel.name), params.tuTunnel.State.UP, config)
+        futureBackend.await().setState(
+          tunnel(params.tunnel.name) { state ->
+            scope.launch(Dispatchers.Main) {
+              Log.i(TAG, "onStateChange - $state")
+              methodChannel?.invokeMethod(
+                "onStateChange",
+                Klaxon().toJsonString(
+                  StateChangeData(params.tunnel.name, state == Tunnel.State.UP)
+                )
+              )
+            }
+          },
+          if (params.state) Tunnel.State.UP else Tunnel.State.DOWN,
+          config
+        )
+        Log.i(TAG, "handleSetState - success!")
+        flutterSuccess(result, true)
+      } catch (e: BackendException) {
+        Log.e(TAG, "handleSetState - BackendException - ERROR - ${e.reason}")
+        flutterError(result, e.reason.toString())
+      } catch (e: Throwable) {
+        Log.e(TAG, "handleSetState - Can't set tunnel state: $e, ${Log.getStackTraceString(e)}")
+        flutterError(result, e.message.toString())
+      }
+    }
+  }
+
+  private fun handleGetNames(result: MethodChannel.Result) {
+    scope.launch(Dispatchers.IO) {
+      try {
+        val names = futureBackend.await().runningTunnelNames
+        Log.i(TAG, "Success $names")
+        flutterSuccess(result, names.toString())
+      } catch (e: Throwable) {
+        Log.e(TAG, "Can't get tunnel names: " + e.message + " " + e.stackTrace)
+        flutterError(result, "Can't get tunnel names")
+      }
+    }
+  }
+
+  private fun handleGetStats(arguments: Any?, result: MethodChannel.Result) {
+    val tunnelName = arguments?.toString()
+    if (tunnelName == null || tunnelName.isEmpty()) {
+      flutterError(result, "Provide tunnel name to get statistics")
+      return
     }
 
-    //@JvmStatic
-    //suspend fun getBackend() = get().futureBackend.await()
+    scope.launch(Dispatchers.IO) {
 
-    @JvmStatic
-    fun getModuleLoader() = get().moduleLoader
+      try {
+        val stats = futureBackend.await().getStatistics(tunnel(tunnelName))
 
-    @JvmStatic
-    fun getRootShell() = get().rootShell
+        flutterSuccess(result, Klaxon().toJsonString(
+          Stats(stats.totalRx(), stats.totalTx())
+        ))
 
-    //@JvmStatic
-    //fun getPreferencesDataStore() = get().preferencesDataStore
+      } catch (e: BackendException) {
+        Log.e(TAG, "handleGetStats - BackendException - ERROR - ${e.reason}")
+        flutterError(result, e.reason.toString())
+      } catch (e: Throwable) {
+        Log.e(TAG, "handleGetStats - Can't get stats: $e")
+        flutterError(result, e.message.toString())
+      }
+    }
+  }
 
-    @JvmStatic
-    fun getToolsInstaller() = get().toolsInstaller
-
-    //@JvmStatic
-    //fun getTunnelManager() = get().tunnelManager
-
-    @JvmStatic
-    fun getCoroutineScope() = get().scope
-
+  /**
+   * Return existing [MyTunnel] from the [tunnels], or create new, add to the list and return it
+   */
+  private fun tunnel(name: String, callback: StateChangeCallback? = null): Tunnel {
+    return tunnels.getOrPut(name, { MyTunnel(name, callback) })
   }
 
 }
 
-class MyTunnel(private val name: String) : Tunnel {
+typealias StateChangeCallback = (Tunnel.State) -> Unit
+
+class MyTunnel(private val name: String,
+               private val onStateChanged: StateChangeCallback? = null) : Tunnel {
 
   override fun getName() = name
 
   override fun onStateChange(newState: Tunnel.State) {
-    Log.i("MyTunnel", "onStateChange $newState")
+    onStateChanged?.invoke(newState)
   }
 
 }
+
+class SetStateParams(
+  val state: Boolean,
+  val tunnel: TunnelData
+)
+
+class TunnelData(
+  val name: String,
+  val address: String,
+  val listenPort: String,
+  val dnsServer: String,
+  val privateKey: String,
+  val peerAllowedIp: String,
+  val peerPublicKey: String,
+  val peerEndpoint: String,
+)
+
+class StateChangeData(
+  val tunnelName: String,
+  val tunnelState: Boolean,
+)
+
+class Stats(
+  val totalDownload: Long,
+  val totalUpload: Long,
+)
