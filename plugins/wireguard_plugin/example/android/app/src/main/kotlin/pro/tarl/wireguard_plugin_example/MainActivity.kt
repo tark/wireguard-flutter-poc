b29c70d9b4622dca65d25b7bb1e7fb657b9fd420
cepuchino <app@test.com>
2021-04-05 11:57:25 +0300

    removed files

--- plugins/wireguard_plugin/example/android/app/src/main/kotlin/pro/tarl/wireguard_plugin_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package pro.tarl.wireguard_plugin_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- plugins/wireguard_plugin/ios/Classes/WireguardPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface WireguardPlugin : NSObject<FlutterPlugin>
-@end
